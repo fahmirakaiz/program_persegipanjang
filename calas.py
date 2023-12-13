626d81e8a4a2cd781eef120b740ce4b912c8eadf
Fahmi Akhtar Rakaiz <74514493+fahmirakaiz@users.noreply.github.com>
2023-12-13 10:03:07 +0700

    Add files via upload
    Program Menghitung Persegi Panjang dengan Pyhton

---
+++ calas.py
@@ -0,0 +1,15 @@
+#Fahmi Akhtar Rakaiz
+
+#Input Nilai
+print("Menghitung Rumus Persegi Panjang")
+
+panjang = int(input("Input Panjang\t : "))
+lebar = int(input("Input Lebar\t : "))
+
+#rumus
+k = 2*(panjang + lebar)
+l = panjang * lebar
+
+#output
+print("Keliling Persegi Panjang\t : ", k)
+print("Luas Persegi Panjang\t\t : ", l)
